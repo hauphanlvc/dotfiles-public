2e2ba8318327e2367e8e671209b71d5b2968368e
hauphanlvc <hauphanlvc@gmail.com>
2023-07-08 04:44:18 +0700

    Add new features.

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,10 +1,14 @@
 local keymap = vim.keymap
-
+-- set nohlsearch
+vim.keymap.set('n', '<leader>k', ':nohlsearch<CR>')
 keymap.set('n', 'x', '"_x')
-
+-- Paste replace visual selection without copying it
+keymap.set('v', 'p', '"_dP')
 -- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
-
+-- Easy insertion of a trailing ; or , from insert mode
+vim.keymap.set('i', ';;', '<Esc>A;<Esc>')
+vim.keymap.set('i', ',,', '<Esc>A,<Esc>')
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
@@ -19,3 +23,7 @@ keymap.set('', 'sh', '<C-w>h')
 keymap.set('', 'sk', '<C-w>k')
 keymap.set('', 'sj', '<C-w>j')
 keymap.set('', 'sl', '<C-w>l')
+-- Maintain the cursor position when yanking a visual selection
+-- http://ddrscott.github.io/blog/2016/yank-without-jank/
+-- vim.keymap.set('v', 'y', 'myy`y')
+-- vim.keymap.set('v', 'Y', 'myY`y')
